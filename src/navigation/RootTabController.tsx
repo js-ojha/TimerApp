e41745ff57839b278c15b40ed119b433dccdf434
Vikas <kuvikasar112@gmail.com>
2025-07-11 14:36:15 +0530

    bottom tab icons add

--- src/navigation/RootTabController.tsx
+++ src/navigation/RootTabController.tsx
@@ -1,12 +1,52 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {
+  BottomTabBar,
+  BottomTabBarProps,
+  createBottomTabNavigator,
+} from '@react-navigation/bottom-tabs';
 import home from '../screens/home/home';
 import history from '../screens/history/history';
+import tw from '../lib/tailwind';
+import { useTheme } from '../provider/ThemeProvider';
+import { Home as HomeIcon, History as HistoryIcon } from '../utils/icons';
 
 const Tab = createBottomTabNavigator();
 
+const TarBarIcon = ({
+  name,
+  color,
+  size,
+}: {
+  color: string;
+  size: number;
+  name: string;
+}) => {
+  switch (name) {
+    case 'Home':
+      return <HomeIcon color={color} size={size} />;
+    case 'History':
+      return <HistoryIcon color={color} size={size} />;
+  }
+};
+
 const RootTabController = () => {
+  const { colors, dark } = useTheme();
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        // eslint-disable-next-line react/no-unstable-nested-components
+        tabBarIcon: ({ color, size }) => (
+          <TarBarIcon name={route.name} color={color} size={size} />
+        ),
+        headerShown: false,
+        tabBarLabelStyle: tw.style('font-normal'),
+        tabBarStyle: {
+          backgroundColor: colors.background,
+          borderTopColor: colors.border,
+        },
+      })}
+      // eslint-disable-next-line react/no-unstable-nested-components
+      tabBar={(props: BottomTabBarProps) => <BottomTabBar {...props} />}
+    >
       <Tab.Screen name="Home" component={home} />
       <Tab.Screen name="History" component={history} />
     </Tab.Navigator>
